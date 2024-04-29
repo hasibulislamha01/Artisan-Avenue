0c67a9d8583fc99a5362b0e0ce32d74b0681cabd
hasibulislamha01 <hasibulislamha01@gmail.com>
2024-04-30 02:13:03 +0600

    edited details page added

--- src/AllSpotPage/SpotDetail.jsx
+++ src/AllSpotPage/SpotDetail.jsx
@@ -15,29 +15,37 @@ const SpotDetail = () => {
             <div className="mt-6 space-y-6">
                 <div className="space-y-2">
                     <h1 className="text-3xl font-semibold">{spot?.spotName}</h1>
-                    <h3 className="text-lg font-medium">Situated in
-                        <span> {spot?.location}</span> ,
+                    <h3 className="text-lg font-medium">
+
                         <span>{spot?.countryName}</span>
                     </h3>
                     <hr className="mb-3 w-full lg:w-1/2  mx-auto" />
                 </div>
 
                 <div className="space-y-2">
-                    <h1 className="text-xl font-medium">About the place</h1>
                     <p className="w-full lg:w-1/2 mx-auto text-lg">
                         {spot?.description}
                     </p>
                 </div>
                 <div className="space-y-2">
-                    <h1 className="text-xl font-medium">Best time to visit</h1>
-                    <p className="w-full lg:w-1/2 mx-auto text-lg">
-                        {spot?.season}
+                    <div className="w-full lg:w-1/2 mx-auto text-lg">
+                        {
+                            spot?.inStock ?
+                                <p>Available in Stock</p>
+                                : <p>Out of Stock</p>
+                        }
+                    </div>
+                    <p>
+                        {
+                            spot?.customizable ? 
+                            <p>Not Customizable</p> :
+                            <p>Customizable</p>
+                        }
                     </p>
                 </div>
                 <div className="space-y-3">
-                    <h2 className="text-xl">On average {spot?.visitors} visit the place in a calender year.</h2>
-                    <h4 className="text-xl">Time Required to visit the place: {spot?.travelDuration}</h4>
-                    <h3 className="text-xl">Expected cost per trip per head: {spot?.cost} $ </h3>
+                    <h4 className="text-xl">Time Required to deliver: {spot?.travelDuration}</h4>
+                    <h3 className="text-xl">Price: {spot?.cost} $ </h3>
                 </div>
             </div>
             <p>added by: {spot?.email}</p>
